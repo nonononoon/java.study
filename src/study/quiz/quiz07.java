b85ff153352d651819f2e684c63e84cb4819b07a
이름 <10443_hr@naver.com>
2024-10-18 17:26:17 +0900

    연산자 연습

---
+++ src/study/quiz/quiz07.java
@@ -0,0 +1,97 @@
+package study.quiz;
+
+import java.util.Scanner;
+
+public class quiz07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*
+		// 1번문제 
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("밑변");
+		int a = scanner.nextInt();
+		
+		int nextInt
+		string nextLine
+		
+		System.out.println("밑변 : " + a);
+		
+		System.out.print("높이");
+		int b = scanner.nextInt();
+		
+		System.out.println("높이 : " + b);
+
+		System.out.println("삼각형의 넓이 = " + (double)a*(double)b/2 + "입니다." );
+		
+		
+		// 2번문제
+		System.out.print("윗변");
+		int c = scanner.nextInt();
+		
+		System.out.print("밑변");
+		int d = scanner.nextInt();
+	
+		
+		System.out.print("높이");
+		int e = scanner.nextInt();
+
+		System.out.printf(" 사다리꼴의 넓이 = " + (c+d)*e/2.0);
+		
+		
+	/* ┌	
+		// A 와 B 나누고 나서 남는 금액 계산 방법
+														    	
+		int people = 24;
+		int total = 100;
+		
+		System.out.println(" 한명당" + (total/people) + "개 과자를 나누면" );
+		System.out.println(" 남는과자는 : " + (total%people) + "개 입니다");
+		                                                                ┘ */
+		
+   /* ┌
+		// 백의자리 십의자리 일의자리 나타내는 방법    
+
+				int num = 158 ;
+		
+		        // 158
+				// 158 / 100 = 1.58 -> 정수 변환 1
+				// 158 % 100 = 58   58/10=5.8 정수변환 5
+				// 158 / 10 = 15.8 -> 15
+				// 158 % 10 = 8
+		
+		System.out.println("백의자리 : " + (num/100) );
+		
+		System.out.println("십의자리 : " + (num/10%10));
+		
+		System.out.println("일의자리 : " + (num%10) );
+															   ┘ */
+
+
+	/* ┌	
+			// 두 변수 교환 하는 방법 	
+			int x = 10;
+			int y = 20;
+			
+			int z = x ;
+			x = y ;
+			y = z ;
+			System.out.println("x :" + x + " y :" + y ) ; 
+		
+			int x = 3;
+			int y = 5;
+			
+			int z = x ;
+			x = y ;
+			y = z ; 
+			System.out.println("x : " + x  + "y : " + y); 
+		 											         ┘ */		
+		
+	
+		
+		
+			
+	}
+
+}
---
+++ src/study/quiz/quiz08.java
@@ -0,0 +1,162 @@
+package study.quiz;
+
+public class quiz08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*	
+		 int time = 11;
+		
+		 // 정상범위 1~24
+		if(time >= 1 && time <= 24) {
+			
+			if(time <= 12) { // 1~12  // 오전 
+				if(time <= 6) { 
+					System.out.println("오전입니다");
+					System.out.println("이른 오전");
+		     } else { // 13~24
+				System.out.println("오전입니다");
+				System.out.println("늦은 오전"); 
+		     }
+			else { 
+				System.out.println("오후입니다");
+				if(time <= 18) {
+					System.out.println("이른 오후");
+				} else {
+					System.out.println("늦은 오후");
+				}
+			}
+			} else {
+			System.out.println("잘못입력했습니다");
+		}
+		
+		
+
+		
+	        // switch 변경 
+				int localNum = 02;
+				
+				switch(localNum) {
+				case 031:
+					System.out.println("경기도입니다");
+					break;
+				case 02:
+					System.out.println("서울입니다.");
+					break;
+				case 041:
+					System.out.println("충남입니다");
+					break;
+				case 051:
+					System.out.println("부산입니다");
+					break;  
+				}
+				 
+				
+				int score = (int)(Math.random()*4)+100; 
+				
+				switch(100) {
+				
+				case 90 :
+				case 91 : 
+				case 92 :
+				case 93 :
+				case 94 : 
+				case 95 :
+				case 96 :
+				case 97 : 
+				case 98 :
+				case 99 :
+				case 100 :
+				 System.out.println("학점 A");
+				break;
+			 
+				case 80 :
+				case 81 :
+				case 82 :
+				case 83 :
+				case 84 :
+				case 85 :
+				case 86 :
+				case 87 :
+				case 88 :
+				case 89 :
+				 System.out.println("학점 B");
+				break;
+				 
+				case 70 :
+				case 71 :
+				case 72 :
+				case 73 :
+				case 74 :
+				case 75 :
+				case 76 :
+				case 77 :
+				case 78 :
+				case 79 :
+					 System.out.println("학점 C");
+				break;
+				default :
+					System.out.println("학점 F");
+				}
+				
+	
+*/
+                int diceNum = 7;
+				
+				if (diceNum ==1) {
+					System.out.println("1");
+				}
+				if (diceNum ==2) {
+					System.out.println("2");
+				}
+				if (diceNum ==3) {
+					System.out.println("3");
+				}
+				if (diceNum ==4) {
+					System.out.println("4");
+				}
+				if (diceNum ==5) {
+					System.out.println("5");
+				}
+				if (diceNum ==6) {
+					System.out.println("6");
+				}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
